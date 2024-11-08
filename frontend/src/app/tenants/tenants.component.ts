23f4c25df073f03bfe6c8f43799861fe4cfc2c8c
Pedro Pereira <pedro.pereira@daslab.de>
2024-11-08 17:52:02 +0100

    chore(tenants-ui): refresh tenants list after creating a tenant

--- frontend/src/app/tenants/tenants.component.ts
+++ frontend/src/app/tenants/tenants.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MatDialog} from "@angular/material/dialog";
 import {AddTenantComponent} from "../add-tenant/add-tenant.component";
@@ -6,27 +6,43 @@ import {AddTenantComponent} from "../add-tenant/add-tenant.component";
 @Component({
   selector: 'app-tenants',
   templateUrl: './tenants.component.html',
-  styleUrls: ['./tenants.component.scss']
+  styleUrls: ['./tenants.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TenantsComponent {
   public tenants: any
   private http = inject(HttpClient)
 
-  constructor(http: HttpClient, public dialog: MatDialog) {
-    http.get("http://localhost:3000/show-tenants").subscribe((tenants) => {
-      this.tenants = tenants;
-    })
+  constructor(
+    http: HttpClient,
+    public dialog: MatDialog,
+    private cd: ChangeDetectorRef
+  ) {
+    this.loadTenants();
   }
 
+  loadTenants() {
+    this.http.get("http://localhost:3000/show-tenants").subscribe((tenants) => {
+      this.tenants = tenants;
+      this.cd.detectChanges();
+    });
+  }
 
   addTenant() {
-    this.dialog.open(AddTenantComponent);
+    const dialogRef = this.dialog.open(AddTenantComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.loadTenants();
+      }
+    });
   }
 
   deleteTenant(tenant: any) {
-    this.tenants.splice(this.tenants.indexOf(tenant), 1);
     this.http.post("http://localhost:3000/delete-tenant?name=" + tenant.name, {}).subscribe((res) => {
-      console.log(res);
-    })
+      const tenants = [...this.tenants];
+      tenants.splice(tenants.indexOf(tenant), 1);
+      this.tenants = tenants;
+      this.cd.detectChanges();
+    });
   }
 }
