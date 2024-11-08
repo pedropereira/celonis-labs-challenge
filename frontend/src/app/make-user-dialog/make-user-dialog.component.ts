0f5f41fc23fdf9f08ac006ad74b8031e0e1e71c0
Pedro Pereira <pedro.pereira@daslab.de>
2024-11-08 16:12:01 +0100

    chore(frontend): fix user creation by sending tenant information to the backend API

--- frontend/src/app/make-user-dialog/make-user-dialog.component.ts
+++ frontend/src/app/make-user-dialog/make-user-dialog.component.ts
@@ -1,5 +1,11 @@
-import {ChangeDetectionStrategy, Component, inject, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {MatDialogRef} from '@angular/material/dialog';
+
+interface Tenant {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-make-user-dialog',
@@ -7,17 +13,53 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./make-user-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MakeUserDialogComponent {
-  public name: any
-  public email: any
-  public userCreated = false
-  @ViewChild('createButton') createButton: any
-  private http = inject(HttpClient)
+export class MakeUserDialogComponent implements OnInit {
+  name = '';
+  email = '';
+  selectedTenantId = '';
+  tenants: Tenant[] = [];
+  userCreated = false;
+  isLoading = false;
+
+  constructor(
+    private dialogRef: MatDialogRef<MakeUserDialogComponent>,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.loadTenants();
+  }
+
+  loadTenants() {
+    this.http.get<Tenant[]>('http://localhost:3000/show-tenants').subscribe(
+      (tenants) => {
+        this.tenants = tenants;
+      },
+      (error) => {
+        console.error('Error loading tenants:', error);
+      }
+    );
+  }
 
   createUser() {
-    this.createButton._elementRef.nativeElement.disabled = true;
-    this.http.get("http://localhost:3000/make-user/" + this.email + "?name=" + this.name).subscribe((users) => {
-      this.userCreated = true;
-    })
+    this.isLoading = true;
+
+    this.http.get(`http://localhost:3000/make-user/${this.email}`, {
+      params: {
+        name: this.name,
+        tenantId: this.selectedTenantId
+      }
+    }).subscribe(
+      () => {
+        this.userCreated = true;
+        setTimeout(() => {
+          this.dialogRef.close(true);
+        }, 1500);
+      },
+      (error) => {
+        console.error('Error creating user:', error);
+        this.isLoading = false;
+      }
+    );
   }
 }
