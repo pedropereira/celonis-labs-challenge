7cc2163a68117064dba0defe55925c3344fb514d
Pedro Pereira <pedro.mdp@gmail.com>
2024-11-08 01:11:01 +0100

    Introduce e2e testing for the API (#4)
    * chore(deps): add jest and dotenv packages
    
    * chore(jest): introduce a new configuration for dealing with ts-based test files
    
    * chore(backend): create a more modular structure for application setup and routing
    
    * chore(docker): remove unused env var in docker-compose.yml
    
    * chore(tests): create a .env.test to allow defining a separate test DB
    
    * chore(tests): create e2e tests for some of the API endpoints
    
    * chore(tests): create helpful commands for running test suite

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import { setupRoutes } from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+setupRoutes(app);
+
+export { app };
---
+++ backend/src/routes.ts
@@ -0,0 +1,68 @@
+import { Express } from 'express';
+import { PrismaClient } from "@prisma/client";
+import { prisma } from './prisma';
+
+export function setupRoutes(app: Express) {
+  app.get('/', (req: any, res: any) => {
+    res.send('Successful response.');
+  });
+
+  app.get('/make-user/:email', (req: any, res: any) => {
+    var user = {email: req.params.email, name: req.query.name};
+    var prisma = new PrismaClient();
+    prisma.user.create({data: user}).then(() => res.json({status: "success"}))
+  
+  });
+
+  app.get('/list-users', (req: any, res: any) => {
+    prisma.user.findMany().then((users) => res.json(users));
+  });
+
+  app.get('/list-users/:name', (req: any, res: any) => {
+    prisma.user.findMany({where: {Tenant: {name: req.params.name}}}).then((users) => res.json(users));
+  });
+
+  app.get('/show-user/:id', (req: any, res: any) => {
+    prisma.user.findUnique({where: {id: req.params.id}}).then((user) => res.json(user));
+  });
+
+  app.get('/send-user/:email', (req: any, res: any) => {
+    prisma.user.findUnique({where: {email: req.params.email}}).then((user) => res.json(user));
+  });
+
+  app.get('/send-user-tenant/:email', (req: any, res: any) => {
+    prisma.user.findUnique({where: {email: req.params.email}}).then((user) => prisma.tenant.findUnique({
+        where: {id: user?.tenantId!}
+    }).then((tenant) => res.json(tenant)).catch((err) => res.json({status: "error", error: err})));
+  });
+
+  app.get('/make-tenant/:name', (req: any, res: any) => {
+    var tenant = {name: req.params.name};
+    prisma.tenant.create({data: tenant}).then(() => res.json({status: "success"}))
+  });
+
+  app.put('/put-user-to-tenant/:email/:name', async (req: any, res: any) => {
+    const tenant = await prisma.tenant.findFirst({where: {name: req.params.name}});
+    prisma.user.update({
+        where: {email: req.params.email},
+        // @ts-ignore
+        data: {Tenant: {connect: {id: tenant.id}}}
+    }).then(() => res.json({status: "success"}))
+  });
+
+  app.get('/show-tenants', (req: any, res: any) => {
+    prisma.tenant.findMany().then((tenants) => res.json(tenants));
+  });
+  
+  app.put('/update-user/:id', (req: any, res: any) => {
+    prisma.user.update({where: {id: req.params.id}, data: {name: req.query.name}}).then(() => res.json({status: "success"}))
+  });
+
+  app.post('/delete-user',(req: any, res: any) => {
+    prisma.user.delete({where: {email: req.query.email}}).then(() => res.json({status: "success"}))
+  });
+
+  app.post('/delete-tenant',(req: any, res: any) => {
+    prisma.tenant.deleteMany({where: {name: req.query.name}}).then(() => res.json({status: "success"}))
+  });
+}
