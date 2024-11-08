26d79957ae23b8ef149878214b655ba6e2a40648
Pedro Pereira <pedro.pereira@daslab.de>
2024-11-08 16:01:46 +0100

    chore(routes): implement repository pattern in order to encapsulate user and tenant db operations

---
+++ backend/src/repositories/user.repository.ts
@@ -0,0 +1,44 @@
+import { User } from "@prisma/client";
+import { prisma } from "../prisma";
+
+export class UserRepository {
+  async create(data: { email: string; name: string; tenantId: string }): Promise<User> {
+    return prisma.user.create({ data });
+  }
+
+  async findMany(): Promise<User[]> {
+    return prisma.user.findMany();
+  }
+
+  async findManyByTenantName(tenantName: string): Promise<User[]> {
+    return prisma.user.findMany({
+      where: { Tenant: { name: tenantName } },
+    });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { id } });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { email } });
+  }
+
+  async update(id: string, data: Partial<User>): Promise<User> {
+    return prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async updateByEmail(email: string, data: Partial<User>): Promise<User> {
+    return prisma.user.update({
+      where: { email },
+      data,
+    });
+  }
+
+  async deleteByEmail(email: string): Promise<User> {
+    return prisma.user.delete({ where: { email } });
+  }
+} 
