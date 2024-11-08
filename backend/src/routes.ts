d9ab3a5827cb3a533822ee528fcdedd9db5ad80c
Pedro Pereira <pedro.pereira@daslab.de>
2024-11-08 12:26:31 +0100

    chore(routes): rename req and res objects to request and response

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,121 +4,122 @@ import * as schemas from "./schemas";
 import { validateRequest } from "./middleware";
 
 export function setupRoutes(app: Express) {
-  app.get("/", (req: Request, res: Response) => {
-    res.send("Successful response.");
+  app.get("/", (_request: Request, response: Response) => {
+    response.status(200).json({ healthy: true });
   });
 
   app.get(
     "/make-user/:email",
-    (req: Request, res: Response, next: NextFunction) => {
-      validateRequest(schemas.makeUserSchema)(req, res, next).catch(next);
+    (request: Request, response: Response, next: NextFunction) => {
+      validateRequest(schemas.makeUserSchema)(request, response, next).catch(next);
     },
-    async (req: Request, res: Response) => {
+    async (request: Request, response: Response) => {
       const userData = {
-        email: req.params.email,
-        name: req.query.name as string,
-        tenantId: req.query.tenantId as string,
+        email: request.params.email,
+        name: request.query.name as string,
+        tenantId: request.query.tenantId as string,
       };
 
       try {
         const user = await prisma.user.create({ data: userData });
 
-        res.status(200).json(user);
+        response.status(200).json(user);
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       }
     }
   );
 
-  app.get("/list-users", async (req: Request, res: Response) => {
+  app.get("/list-users", async (_request: Request, response: Response) => {
     try {
       const users = await prisma.user.findMany();
 
-      res.status(200).json(users);
+      response.status(200).json(users);
     } catch (error) {
       const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-      res.status(400).json({ status: "error", error: errorMessage });
+      response.status(400).json({ status: "error", error: errorMessage });
     }
   });
 
-  app.get("/list-users/:name", async (req: Request, res: Response) => {
+  app.get("/list-users/:name", async (request: Request, response: Response) => {
     await prisma.user
-      .findMany({ where: { Tenant: { name: req.params.name } } })
-      .then((users) => res.status(200).json(users))
+      .findMany({ where: { Tenant: { name: request.params.name } } })
+      .then((users) => response.status(200).json(users))
       .catch((error) => {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       });
   });
 
-  app.get("/show-user/:id", async (req: Request, res: Response) => {
+  app.get("/show-user/:id", async (request: Request, response: Response) => {
     await prisma.user
-      .findUnique({ where: { id: req.params.id } })
-      .then((user) => res.status(200).json(user))
+      .findUnique({ where: { id: request.params.id } })
+      .then((user) => response.status(200).json(user))
       .catch((error) => {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       });
   });
 
-  app.get("/send-user/:email", async (req: Request, res: Response) => {
+  app.get("/send-user/:email", async (request: Request, response: Response) => {
     await prisma.user
-      .findUnique({ where: { email: req.params.email } })
-      .then((user) => res.status(200).json(user))
+      .findUnique({ where: { email: request.params.email } })
+      .then((user) => response.status(200).json(user))
       .catch((error) => {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       });
   });
 
-  app.get("/send-user-tenant/:email", async (req: Request, res: Response) => {
+  app.get("/send-user-tenant/:email", async (request: Request, response: Response) => {
     await prisma.user
-      .findUnique({ where: { email: req.params.email } })
+      .findUnique({ where: { email: request.params.email } })
       .then((user) => {
         if (user?.tenantId) {
           return prisma.tenant
             .findUnique({
               where: { id: user.tenantId },
             })
-            .then((tenant) => res.json(tenant));
+            .then((tenant) => response.json(tenant));
         } else {
-          res.status(404).json({ status: "error", error: "User or tenantId not found" });
+          response.status(404).json({ status: "error", error: "User or tenantId not found" });
         }
       })
       .catch((error) => {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       });
   });
 
-  app.get("/make-tenant/:name", async (req: Request, res: Response) => {
-    const tenant = { name: req.params.name };
+  app.get("/make-tenant/:name", async (request: Request, response: Response) => {
+    const tenant = { name: request.params.name };
 
     await prisma.tenant
       .create({ data: tenant })
-      .then(() => res.status(200).json({ status: "success" }));
+      .then(() => response.status(200).json({ status: "success" }));
   });
 
   app.put(
     "/put-user-to-tenant/:email/:name",
-    (req: Request, res: Response, next: NextFunction) => {
-      validateRequest(schemas.putUserToTenantSchema)(req, res, next).catch(next);
+    (request: Request, response: Response, next: NextFunction) => {
+      validateRequest(schemas.putUserToTenantSchema)(request, response, next).catch(next);
     },
-    async (req: Request, res: Response) => {
-      const { email, name } = req.params;
+    async (request: Request, response: Response) => {
+      const { email, name } = request.params;
 
       try {
         const tenant = await prisma.tenant.findFirst({ where: { name } });
 
         if (!tenant) {
-          res.status(404).json({ status: "error", error: "Tenant not found" });
+          response.status(404).json({ status: "error", error: "Tenant not found" });
+
           return;
         }
 
@@ -127,35 +128,35 @@ export function setupRoutes(app: Express) {
           data: { Tenant: { connect: { id: tenant.id } } },
         });
 
-        res.status(200).json({ status: "success" });
+        response.status(200).json({ status: "success" });
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       }
     }
   );
 
-  app.get("/show-tenants", async (req: Request, res: Response) => {
+  app.get("/show-tenants", async (_request: Request, response: Response) => {
     await prisma.tenant
       .findMany()
-      .then((tenants) => res.status(200).json(tenants))
+      .then((tenants) => response.status(200).json(tenants))
       .catch((error) => {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       });
   });
 
   app.put(
     "/update-user/:id",
-    (req: Request, res: Response, next: NextFunction) => {
-      validateRequest(schemas.updateUserSchema)(req, res, next).catch(next);
+    (request: Request, response: Response, next: NextFunction) => {
+      validateRequest(schemas.updateUserSchema)(request, response, next).catch(next);
     },
-    async (req: Request, res: Response) => {
-      const userId = req.params.id;
+    async (request: Request, response: Response) => {
+      const userId = request.params.id;
       const userData = {
-        name: req.query.name as string,
+        name: request.query.name as string,
       };
 
       try {
@@ -164,51 +165,51 @@ export function setupRoutes(app: Express) {
           data: userData,
         });
 
-        res.status(200).json(user);
+        response.status(200).json(user);
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       }
     }
   );
 
   app.post(
     "/delete-user",
-    (req: Request, res: Response, next: NextFunction) => {
-      validateRequest(schemas.deleteUserSchema)(req, res, next).catch(next);
+    (request: Request, response: Response, next: NextFunction) => {
+      validateRequest(schemas.deleteUserSchema)(request, response, next).catch(next);
     },
-    async (req: Request, res: Response) => {
-      const userEmail = req.query.email as string;
+    async (request: Request, response: Response) => {
+      const userEmail = request.query.email as string;
 
       try {
         await prisma.user.delete({ where: { email: userEmail } });
 
-        res.status(200).json({ status: "success" });
+        response.status(200).json({ status: "success" });
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       }
     }
   );
 
   app.post(
     "/delete-tenant",
-    (req: Request, res: Response, next: NextFunction) => {
-      validateRequest(schemas.deleteTenantSchema)(req, res, next).catch(next);
+    (request: Request, response: Response, next: NextFunction) => {
+      validateRequest(schemas.deleteTenantSchema)(request, response, next).catch(next);
     },
-    async (req: Request, res: Response) => {
-      const tenantName = req.query.name as string;
+    async (request: Request, response: Response) => {
+      const tenantName = request.query.name as string;
 
       try {
         await prisma.tenant.deleteMany({ where: { name: tenantName } });
 
-        res.status(200).json({ status: "success" });
+        response.status(200).json({ status: "success" });
       } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "Unknown error";
 
-        res.status(400).json({ status: "error", error: errorMessage });
+        response.status(400).json({ status: "error", error: errorMessage });
       }
     }
   );
